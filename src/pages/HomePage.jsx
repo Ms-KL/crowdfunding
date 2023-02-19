a8158d03c269c78bc59e280afc26d011a348f0e3
Ms-KL-Codes <thekristyleigh@gmail.com>
2023-02-19 10:34:09 +0800

    $ sign added to pledges total + notes moved to base of code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,7 +19,6 @@ function HomePage() {
 
   // Project Data
   useEffect(() => {
-    // REQUEST DATA + PROMISE
     fetch(`${import.meta.env.VITE_API_URL}projects`)
       .then((results) => {
         return results.json();
@@ -31,7 +30,6 @@ function HomePage() {
     
   // Pledge Data
   useEffect(() => {
-    // REQUEST DATA + PROMISE
     fetch(`${import.meta.env.VITE_API_URL}pledges`)
       .then((results) => {
         return results.json();
@@ -39,15 +37,6 @@ function HomePage() {
       .then((data) => {
         setPledgeList(data);
       });
-
-    // fetch: built in request in browser to server
-      // when the results get back, will receive JSON
-      // JSON key:value pair
-      // When you send data back, we send strings back
-      // when the results return - need to turn it into JSON
-      // set the JSON to the projectList --> async code
-      // fetch, then do this, then do that
-
 
   }, []);
 
@@ -60,11 +49,6 @@ function HomePage() {
     .map(pledge => pledge.amount)
     .reduce((runningTotal, amount) => runningTotal + amount,0);
 
-    // map = create new array that only contains the pledge amounts
-    // reduce = sum up pledge amounts in the new array, return the total
-      // 0 = initial value of reduce = 0
-      // https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d
-  
   return (
     <div>
       <h1>Communitree</h1>
@@ -74,8 +58,7 @@ function HomePage() {
       <p className="kl-to-do">Add Activity Feed - sideways scroll? Feed like ASX?</p>
 
       <h2>Communitree Support Totals</h2>
-      <p>Communitree projects: {projectsTotal} | Communitree pledges: {pledgesTotal} | Communitree funds raised: {pledgesTotalFunds} </p> 
-      <p className="kl-to-do"> need to figure out how to sum a field imported from DRF and return total</p>
+      <p>Total Projects: {projectsTotal} | Total Pledges: {pledgesTotal} | Total Funds Raised: ${pledgesTotalFunds} </p> 
 
       <h3>All Projects</h3>
       <div id="project-list">
@@ -92,10 +75,22 @@ export default HomePage;
 // without default you import using { }
 
 
-
-
-
-
 // Resources:
   // https://stackoverflow.com/questions/54002792/in-general-is-it-better-to-use-one-or-many-useeffect-hooks-in-a-single-component#:~:text=It's%20perfectly%20fine%20to%20have%20have%20multiple%20useEffect.
   // https://stackoverflow.com/questions/62358365/react-js-get-sum-of-numbers-in-array
+
+// NOTES:
+  // 
+    // fetch: built in request in browser to server
+      // when the results get back, will receive JSON
+      // JSON key:value pair
+      // When you send data back, we send strings back
+      // when the results return - need to turn it into JSON
+      // set the JSON to the projectList --> async code
+      // fetch, then do this, then do that
+  //
+    // map = create new array that only contains the pledge amounts
+    // reduce = sum up pledge amounts in the new array, return the total
+      // 0 = initial value of reduce = 0
+      // https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d
+  
