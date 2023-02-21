ddd936317c67326a208e9ce4aabe3669b8592258
Ms-KL-Codes <thekristyleigh@gmail.com>
2023-02-21 19:26:06 +0800

    Login Form and Token Storage

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+function LoginForm() {
+
+    // state 
+
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+    });
+    console.log(LoginForm)
+
+    // HOOKS
+    const navigate = useNavigate();
+
+
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        // grab the id and value from target
+        // every time the input changes it calls this function
+        // event.target: get the target of input.
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            [id]: value,
+        }));
+        // [id] = overrides
+    };
+
+    const postData = async () => {
+        const response = await fetch(
+            `${import.meta.env.VITE_API_URL}api-token-auth/`,
+            {
+                method: "post",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify(credentials),
+            }
+        );
+        return response.json();
+    };
+
+    
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        if (credentials.username && credentials.password) {
+            const { token } = await postData();
+            window.localStorage.setItem("token", token);
+            navigate("/");
+        }
+    };
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <div>
+        <label htmlFor="username">Username:</label>
+        <input
+            type="text"
+            id="username"
+            placeholder="Enter username"
+            onChange={handleChange}
+
+        />
+        </div>
+        <div>
+        <label htmlFor="password">Password:</label>
+        <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={handleChange}
+
+        />
+        </div>
+        <button type="submit">
+        Login
+        </button>
+        </form>
+    );
+}
+
+export default LoginForm;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// components
+
+import LoginForm from "../components/LoginForm/LoginForm";
+
+function LoginPage() {
+    return <LoginForm />;
+}
+
+export default LoginPage;
