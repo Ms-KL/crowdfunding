a348a1740c0074277b59bbbc4e89a8648f552ba9
Ms-KL-Codes <thekristyleigh@gmail.com>
2023-02-22 15:24:32 +0800

    Cleaned up Nav and added Registration form

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,33 +8,19 @@ function Nav(props) {
         setLoggedIn(false)
     }
 
-
-function Nav() {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/login">Login</Link>
-            {/* Base url / origin / domain */}
-            {/* <Link to="/project">Project</Link> */}
-            {/* Path parameter */}
+            <div>
+                {!loggedIn && <Link to="/login" className="btn">Login</Link>}
+                {!loggedIn && <Link to="/register" className="btn">Register</Link>}
+                <div id="nav-controls">
+                    <Link to="/" >Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
+            </div>
+            {loggedIn && <button onClick={handleClick}>Log Out</button>}
         </nav>
     );
-}
-
-return (
-    <nav>
-        <div id="nav-right">
-            {!loggedIn && <Link to="/login" className="btn">Login In</Link>}
-            <div id="nav-controls">
-                <Link to="/" >Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-            </div>
-        </div>
-        {loggedIn && <button onClick={handleClick}>Sign Out</button>}
-    </nav>
-);
 }
 export default Nav;
