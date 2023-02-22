66e1d2f42c8dc48786448ff1efaf4159f49e8040
Ms-KL-Codes <thekristyleigh@gmail.com>
2023-02-22 14:58:41 +0800

    context created to establish logged-in status, pledgepage and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { useState } from "react";
 
 // Pages
 import HomePage from "./pages/HomePage";
@@ -6,31 +7,45 @@ import ProjectPage from "./pages/ProjectPage";
 import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
 import LoginPage from "./pages/LoginPage";
+import PledgePage from "./pages/PledgePage";
 
 // Components
 import Nav from "./components/Nav/nav";
+import Footer from "./components/Footer/Footer";
 
 // CSS
 import "./App.css";
 
 
-const HeaderLayout = () => (
-  <div>
-    <Nav />
-    <Outlet />
-  </div>
-);
+// const HeaderLayout = () => (
+//   <div>
+//     <Nav />
+//     <Outlet />
+//   </div>
+// );
+
+const Layout = () => {
+  const [loggedIn, setLoggedIn] = useState(window.localStorage.getItem("token") != null)
+  return (
+    <>
+      <Nav loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <Outlet context={[loggedIn, setLoggedIn]} />
+      <Footer />
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
-    element: <HeaderLayout />,
+    // element: <HeaderLayout />,
+    element: <Layout />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/project/:id", element: <ProjectPage /> },
       { path: "/about", element: <AboutPage />},
       { path: "/contact", element: <ContactPage />},
       { path: "/login", element: <LoginPage />},
-        
+      { path: "/pledges", element: <PledgePage />},        
     ],
   },
 ]);
