be1b06c124c02d7354bf4edde10c08f27393bdc6
Ms-KL-Codes <thekristyleigh@gmail.com>
2023-02-22 21:57:46 +0800

    Registration form made functional

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,11 +1,103 @@
 import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
+function RegistrationForm() {
+    const [users, setUsers] = useState({
+        "username": "",
+        "email": "",
+        "password": "",
+        "bio": "",
+        "avatar": "",
+    });
 
-function RegistrationForm () {
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setUsers((prevUsers) => ({
+        ...prevUsers,
+        [id]: value,
+        }));
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const authToken = window.localStorage.getItem("token")
+
+        if (!authToken) {
+            try {
+                const response = await fetch(
+                    `${import.meta.env.VITE_API_URL}users/`,
+                    {
+                    method: "post",
+                    headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(users),
+                }
+            );
+            navigate(`/login`);
+        } catch (err) {
+            console.error(err);
+        }
+    } else {
+    // redirect to login page
+    navigate(`/`);
+    }
+    };
+
     return (
-        <>
-            <h1>This is the registration form</h1>
-        </>
+        <div>
+        <form onSubmit={handleSubmit}>
+            <div>
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                placeholder="Enter unique username"
+                onChange={handleChange}
+            />
+            </div>
+            <div>
+            <label htmlFor="email">Email:</label>
+            <input
+                type="email"
+                id="email"
+                placeholder="Enter unique email"
+                onChange={handleChange}
+            />
+            </div>
+            <div>
+            <label htmlFor="password">Password:</label>
+            <input
+                type="text"
+                id="password"
+                placeholder="Enter password"
+                onChange={handleChange}
+            />
+            </div>
+            <div>
+            <label htmlFor="bio">Bio:</label>
+            <input
+                type="text"
+                id="bio"
+                placeholder="enter Bio"
+                onChange={handleChange}
+            />
+            </div>
+            <div>
+            <label htmlFor="avatar">Avatar Image URL:</label>
+            <input 
+                type="text"
+                id="avatar" 
+                onChange={handleChange} 
+            />
+            </div>
+            <button type="submit">Register User</button>
+        </form>
+        </div>
     );
 }
 
