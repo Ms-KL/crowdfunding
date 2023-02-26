a14f403d6dc6cb068cad8d6292efa11cccd04aff
Ms-KL-Codes <thekristyleigh@gmail.com>
2023-02-27 04:21:16 +0800

    Project Cards on Homepage formatted

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -9,12 +9,24 @@ function ProjectCard(props) {
 
   return (
     <div className="project-card">
-      <Link to={`/project/${projectData.id}`}>
+      <div className="project-image-container">
+        <Link to={`/project/${projectData.id}`}>
         <img src={projectData.image} />
+        </Link>
+      </div>
+      <div className="project-card-text">
         <h3>{projectData.title}</h3>
-      </Link>
+        <p>{projectData.description}</p>
+        <h4>${projectData.sum_pledges} raised  &emsp; {projectData.funding_status}</h4>
+
+      </div>
     </div>
   );
 }
 
 export default ProjectCard;
+
+
+{/* <h3>{project.funding_status} Project!</h3>
+<h4>Goal: ${project.goal} | Total Pledges: ${project.sum_pledges}</h4>
+<h4>Balance: ${project.goal_balance}</h4> */}
